6d0cc99161cb71b113c3ef108c4d6c720c93f829
arthur tavares <arthur_vtavares@hotmail.com>
2018-09-15 17:47:52 -0300

    v4

--- src/test/java/school/cesar/unit/EmailClientTest.java
+++ src/test/java/school/cesar/unit/EmailClientTest.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import java.util.ArrayList;
+
 
 
     public class EmailClientTest {
@@ -76,8 +78,274 @@ import org.junit.jupiter.api.Assertions;
             Assertions.assertTrue(emailClient.isAValidUser("usu4Ri0._vA-lid0"));
         }
 
+        @Test
+        public void testingIsValidAddressCollection_UsingAValidEmailCollection() {
+            to = new ArrayList<String>();
+            ((ArrayList<String>) to).add("arthurvts@hotmail.com");
+            ((ArrayList<String>) to).add("a4t8@hotmail.com");
+            ((ArrayList<String>) to).add("art-hur@hotmail.com");
+            ((ArrayList<String>) to).add("art_hur@hotmail.com");
+            ((ArrayList<String>) to).add("art-hu_r@hotmail.com");
+
+            Assertions.assertTrue(emailClient.isValidAddress(to));
+        }
 
 
+        @Test
+        public void testingIsValidAddressCollection_UsingAnInvalidEmailCollection() {
+            to = new ArrayList<String>();
+            ((ArrayList<String>) to).add("arthur@hotmail.com");
+            ((ArrayList<String>) to).add("a&*@hotmail.com");
+            ((ArrayList<String>) to).add("art..@hotmail.com");
+            ((ArrayList<String>) to).add("@rt_hur@hotmail.com");
+            ((ArrayList<String>) to).add("art-hu_r@hotmail");
+
+            Assertions.assertFalse(emailClient.isValidAddress(to));
+        }
+
+        @Test
+        public void testingIsValidEmail_UsingValidEmailsValidFrom(){
+
+            to = new ArrayList<String>();
+            ((ArrayList<String>) to).add("arthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rt_hur@hotmail.com");
+
+            cc = new ArrayList<String>();
+            ((ArrayList<String>) cc).add("arthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rt_hur@hotmail.com");
+
+            bcc = new ArrayList<String>();
+            ((ArrayList<String>) bcc).add("arthur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rthur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rt_hur@hotmail.com");
+
+            Email email = new EmailBuilder()
+                    .setCreationDate(Instant.now())
+                    .setFrom("arthur@hotmail.com")
+                    .setTo(to)
+                    .setCc(cc)
+                    .setBcc(bcc)
+                    .build();
+
+            Assertions.assertTrue(emailClient.isValidEmail(email));
+        }
+
+        @Test
+        public void testingIsValidEmail_UsingValidEmailsInvalidFrom(){
+
+            to = new ArrayList<String>();
+            ((ArrayList<String>) to).add("arthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rt_hur@hotmail.com");
+
+            cc = new ArrayList<String>();
+            ((ArrayList<String>) cc).add("arthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rt_hur@hotmail.com");
+
+            bcc = new ArrayList<String>();
+            ((ArrayList<String>) bcc).add("arthur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rthur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rt_hur@hotmail.com");
+
+            Email email = new EmailBuilder()
+                    .setCreationDate(Instant.now())
+                    .setFrom("art*hur@hotmail.com")
+                    .setTo(to)
+                    .setCc(cc)
+                    .setBcc(bcc)
+                    .build();
+
+            Assertions.assertFalse(emailClient.isValidEmail(email));
+
+        }
+
+        @Test
+        public void testingIsValidEmail_UsingInvalidEmailsValidFrom(){
+
+            to = new ArrayList<String>();
+            ((ArrayList<String>) to).add("a@rthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rt_hur@hotmail.com");
+
+            cc = new ArrayList<String>();
+            ((ArrayList<String>) cc).add("ar*thur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rt_hur@hotmail.com");
+
+            bcc = new ArrayList<String>();
+            ((ArrayList<String>) bcc).add("arthur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rt._@hur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rt_hur@hotmail.com");
+
+            Email email = new EmailBuilder()
+                    .setCreationDate(Instant.now())
+                    .setFrom("arthur@hotmail.com")
+                    .setTo(to)
+                    .setCc(cc)
+                    .setBcc(bcc)
+                    .build();
+
+            Assertions.assertFalse(emailClient.isValidEmail(email));
+
+        }
+
+        @Test
+        public void testingIsValidEmail_UsingInvalidEmailsInvalidFrom(){
+
+            to = new ArrayList<String>();
+            ((ArrayList<String>) to).add("a@rthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rt_hur@hotmail.com");
+
+            cc = new ArrayList<String>();
+            ((ArrayList<String>) cc).add("ar*thur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rt_hur@hotmail.com");
+
+            bcc = new ArrayList<String>();
+            ((ArrayList<String>) bcc).add("arthur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rt._@hur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rt_hur@hotmail.com");
+
+            Email email = new EmailBuilder()
+                    .setCreationDate(Instant.now())
+                    .setFrom("a@rt@hur@hotmail.com")
+                    .setTo(to)
+                    .setCc(cc)
+                    .setBcc(bcc)
+                    .build();
+
+            Assertions.assertFalse(emailClient.isValidEmail(email));
+
+        }
+
+        @Test
+        public void testingIsValidEmail_UsingValidEmails_WithNoBccNoCc(){
+
+            to = new ArrayList<String>();
+            ((ArrayList<String>) to).add("arthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rt_hur@hotmail.com");
+
+            cc = new ArrayList<String>();
+            bcc = new ArrayList<String>();
+
+            Email email = new EmailBuilder()
+                    .setCreationDate(Instant.now())
+                    .setFrom("arthur@hotmail.com")
+                    .setTo(to)
+                    .setCc(cc)
+                    .setBcc(bcc)
+                    .build();
+
+            Assertions.assertTrue(emailClient.isValidEmail(email));
+
+        }
+
+        @Test
+        public void testingIsValidEmail_UsingValidEmailsValidFrom_WithNoCreationDate(){
+
+            to = new ArrayList<String>();
+            ((ArrayList<String>) to).add("arthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rt_hur@hotmail.com");
+
+            cc = new ArrayList<String>();
+            ((ArrayList<String>) cc).add("arthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rt_hur@hotmail.com");
+
+            bcc = new ArrayList<String>();
+            ((ArrayList<String>) bcc).add("arthur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rthur@hotmail.com");
+            ((ArrayList<String>) bcc).add("4rt_hur@hotmail.com");
+
+            Email email = new EmailBuilder()
+                    .setFrom("arthur@hotmail.com")
+                    .setTo(to)
+                    .setCc(cc)
+                    .setBcc(bcc)
+                    .build();
+
+            Assertions.assertFalse(emailClient.isValidEmail(email));
+        }
+
+        @Test
+        public void testingIsValidEmail_UsingValidEmailsValidFrom_WithNoBccOnly(){
+
+            to = new ArrayList<String>();
+            ((ArrayList<String>) to).add("arthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rthur@hotmail.com");
+            ((ArrayList<String>) to).add("4rt_hur@hotmail.com");
+
+            cc = new ArrayList<String>();
+            ((ArrayList<String>) cc).add("arthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rthur@hotmail.com");
+            ((ArrayList<String>) cc).add("4rt_hur@hotmail.com");
+
+            bcc = new ArrayList<String>();
+
+            Email email = new EmailBuilder()
+                    .setCreationDate(Instant.now())
+                    .setFrom("arthur@hotmail.com")
+                    .setTo(to)
+                    .setCc(cc)
+                    .setBcc(bcc)
+                    .build();
+
+            Assertions.assertTrue(emailClient.isValidEmail(email));
+        }
+
+        @Test
+        public void creatingAnAccountUsingAValidAccount(){
+            emailAccount = new EmailAccountBuilder()
+                    .setUser("Arthur")
+                    .setDomain("hotmail")
+                    .setPassword("987654")
+                    .setLastPasswordUpdate(Instant.now())
+                    .build();
+
+            Assertions.assertTrue(emailClient.createAccount(emailAccount));
+        }
+
+        @Test
+        public void creatingAnAccountUsingAInvalidAccountPassword(){
+            emailAccount = new EmailAccountBuilder()
+                    .setUser("Arthur")
+                    .setDomain("hotmail")
+                    .setPassword("987")
+                    .setLastPasswordUpdate(Instant.now())
+                    .build();
+
+            Assertions.assertFalse(emailClient.createAccount(emailAccount));
+        }
+
+        @Test
+        public void creatingAnAccountUsingAInvalidAccountUser(){
+            emailAccount = new EmailAccountBuilder()
+                    .setUser("@rthu*")
+                    .setDomain("hotmail")
+                    .setPassword("847603")
+                    .setLastPasswordUpdate(Instant.now())
+                    .build();
+
+            Assertions.assertFalse(emailClient.createAccount(emailAccount));
+        }
+
+        @Test
+        public void creatingAnAccountUsingAInvalidAccountDomain(){
+            emailAccount = new EmailAccountBuilder()
+                    .setUser("Arthur")
+                    .setDomain("hotm@il")
+                    .setPassword("847603")
+                    .setLastPasswordUpdate(Instant.now())
+                    .build();
+
+            Assertions.assertFalse(emailClient.createAccount(emailAccount));
+        }
 
 
 
